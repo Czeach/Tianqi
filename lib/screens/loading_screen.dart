1dfee1294a9f7755bc92c233e6c12a8275396fa9
Cynthia Peter <cynthiapeterpearl@gmail.com>
2020-02-14 09:26:28 +0100

    interface done, aside slider

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -40,13 +40,74 @@ class _LoadingScreenState extends State<LoadingScreen> {
             ),
             SizedBox(height: 20.0),
             Container(
-              height: 450,
+              height: MediaQuery.of(context).size.height * 0.68,
               width: 350,
               decoration: BoxDecoration(
-              color: Colors.black54,
-              borderRadius: BorderRadius.circular(25)
+                  color: Colors.black87.withOpacity(0.7),
+                  borderRadius: BorderRadius.circular(25)),
+              child: Stack(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(left: 0.0, top: 0.0, bottom: 0.0),
+                          child: Text(
+                            '30',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 120,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Text(
+                          'Rain',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25.0,
+                              color: Colors.white,
+                              ),
+                        ),
+                        SizedBox(height: 100),
+                        Text('Light rain stoping in ten minutes, you wanna grab your bag, it starts again in 10 mins',
+                        style: TextStyle(fontSize: 15, color: Colors.white70),),
+                        SizedBox(height:50),
+                        Divider(color: Colors.white38,),
+                        SizedBox(height:15),
+                        Padding(
+                          padding: const EdgeInsets.only(left:10.0, right:10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text('28-32', style:TextStyle(color: Colors.white70),),
+                              Icon(Icons.cloud_queue, color: Colors.red,),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height:5,
+                  width:5,
+                  color: Colors.limeAccent[400],
+                ),
+                SizedBox(width:5),
+                Text('Updated 3 mins ago', style: TextStyle(color: Colors.white70, fontSize: 12),),
+              ],
+              ),
+            )
           ],
         ),
       ),
